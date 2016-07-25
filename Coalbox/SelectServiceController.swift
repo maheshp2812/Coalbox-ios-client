53ee47927f1b49c44d409db8ffb02e5d7d4bd0d3
maheshp2812 <maheshp2812@gmail.com>
2016-07-25 15:25:58 +0530

    Date format changed

--- Coalbox/SelectServiceController.swift
+++ Coalbox/SelectServiceController.swift
@@ -77,7 +77,7 @@ class SelectServiceController : UIViewController {
     
     @IBAction func onProceed(sender: UIButton) {
         let formatter = NSDateFormatter()
-        formatter.dateStyle = .ShortStyle
+        formatter.dateFormat = "dd-MM-yyyy"
         var slotString = ""
         if let a = pickupDate {
             orderDetail.setDetail(formatter.stringFromDate(a), forKey: "pickupDate")
